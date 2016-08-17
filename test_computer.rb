4dc4054e42bdc931e4703c387ea7b148805d42fd
Gaku Manabu Kobayashi <manamanmana@gmail.com>
2016-08-17 20:13:33 +0900

    ADDED: test_set_address() method

--- test_computer.rb
+++ test_computer.rb
@@ -1,5 +1,6 @@
 require('test/unit')
 require('./computer')
+require('./errors')
 
 # Test Case for Computer Class
 class TCComputer < Test::Unit::TestCase
@@ -14,5 +15,24 @@ class TCComputer < Test::Unit::TestCase
     assert_equal(100, @computer.memory_stack_size)
     assert_equal(0, @computer.program_counter)
     assert_equal(99, @computer.stack_pointer)
+  end
+
+  def test_set_address
+    # address must be Integer
+    assert_raise(InvalidAddressTypeError) do
+      @computer.set_address('address')
+    end
+    # Address must be range of 0..99
+    assert_raise(InvalidAddressPointerError) do
+      @computer.set_address(-1)
+    end
+    assert_raise(InvalidAddressPointerError) do
+      @computer.set_address(1000)
+    end
+    # Return value of set_address is Computer instanse itself
+    assert_same(@computer, @computer.set_address(1))
+    # set_address method assign address to program counter
+    @computer.set_address(3)
+    assert_equal(3, @computer.program_counter)
   end
 end
